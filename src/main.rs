1633317aa6b08e6948e8c5d13abfc042cbc7e05f
Wilfred Hughes <me@wilfred.me.uk>
2016-07-04 08:31:59 +0100

    REPL loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 /// A value in our little language. For now, just an integer.
 #[derive(Debug)]
 struct Value {
@@ -8,11 +10,25 @@ fn lex(source: &str) -> Vec<Value> {
     vec![Value { value: 42 }]
 }
 
-fn main() {
+fn read() -> String {
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input)
+        .expect("Failed to read line");
+
+    input
+}
+
+fn repl() {
     loop {
         println!("sooty> ");
 
-        println!("{:?}", lex("42"));
+        let input = read();
+        println!("{:?}", lex(&input));
         break;
     }
+}
+
+fn main() {
+    repl();
 }
