995e6a3e0a4073a3e4f8cab3f43ef683eba67428
Wilfred Hughes <me@wilfred.me.uk>
2016-07-04 08:24:14 +0100

    Skeleton for lexing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+/// A value in our little language. For now, just an integer.
+#[derive(Debug)]
+struct Value {
+    value: i64,
+}
+
+fn lex(source: &str) -> Vec<Value> {
+    vec![Value { value: 42 }]
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("sooty> ");
+
+        println!("{:?}", lex("42"));
+        break;
+    }
 }
