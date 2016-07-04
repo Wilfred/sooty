2ca1524eab9b54eaa9b1c566a7ede7dfc4075186
Wilfred Hughes <me@wilfred.me.uk>
2016-07-04 19:19:56 +0100

    Split on whitespace when lexing

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,11 +1,21 @@
 // TODO: move Value to a separate file.
 
 /// A value in our little language. For now, just an integer.
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Value {
     value: i64,
 }
 
 pub fn lex(source: &str) -> Vec<Value> {
-    vec![Value { value: 42 }]
+    let mut result = vec![];
+    for part in source.split_whitespace() {
+        result.push(Value { value: 42 });
+    }
+    result
+}
+
+#[test]
+fn test_lex_multiple_numbers() {
+    let lexed = lex(&"42 42");
+    assert!(lexed == vec![Value { value: 42}, Value { value: 42}]);
 }
