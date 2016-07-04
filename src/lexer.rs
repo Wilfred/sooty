961ac07bd93b992c69b163d740ea516fb70fc8df
Wilfred Hughes <me@wilfred.me.uk>
2016-07-04 23:27:26 +0100

    Actually lex integers

--- src/lexer.rs
+++ src/lexer.rs
@@ -9,13 +9,26 @@ pub struct Value {
 pub fn lex(source: &str) -> Result<Vec<Value>, ()> {
     let mut result = vec![];
     for part in source.split_whitespace() {
-        result.push(Value { value: 42 });
+        match part.parse::<i64>() {
+            Ok(num) => {
+                result.push(Value { value: num });
+            }
+            Err(..) => {
+                return Err(());
+            }
+        }
     }
     Ok(result)
 }
 
 #[test]
+fn test_lex_single_number() {
+    let lexed = lex(&"41").unwrap();
+    assert!(lexed == vec![Value { value: 41 }]);
+}
+
+#[test]
 fn test_lex_multiple_numbers() {
     let lexed = lex(&"42 42").unwrap();
-    assert!(lexed == vec![Value { value: 42}, Value { value: 42}]);
+    assert!(lexed == vec![Value { value: 42 }, Value { value: 42 }]);
 }
